044b21160f3c00e26afb78fd1191b4c543eb2984
javierperezferrada <javierperezferrada@hotmail.com>
2016-03-22 16:10:10 -0300

    actualizado datos de contacto

--- contact_service.php
+++ contact_service.php
@@ -3,7 +3,7 @@
   $message.= "\nNombre: ". $_POST['name'];
   $message.= "\nEmail: ".$_POST['email'];
   $message.= "\nTelefono: ". $_POST['phone'];
-  $message.= "\nmessage: \n".$_POST['message'];
+  $message.= "\nMensaje: \n".$_POST['message'];
   $to= "javier_dc2@hotmail.com";
   $from = $_POST['email'];
   $subject = "Mensaje enviado por: ".$_POST['name'];
