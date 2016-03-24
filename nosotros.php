e1ca84afce6c652b2b1cc5b42da7dc2a68ee7a4f
javierperezferrada <javierperezferrada@hotmail.com>
2016-03-24 12:46:16 -0300

    con include de php
    se cambia el contenido del encabezado y el pie de pagina de todas las
    vistas en un solo archivo.

--- nosotros.php
+++ nosotros.php
@@ -39,39 +39,7 @@
   </head>
 
   <body>
-    <!--header start-->
-    <header class="head-section">
-      <div class="navbar navbar-default navbar-static-top container">
-        <div class="navbar-header">
-          <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index">GyP<span>Network</span></a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a href="/">Inicio</a>
-            </li>
-            <li class="dropdown">
-              <a href="productos">Productos</a>
-            </li>
-            <li class="active">
-              <a href="nosotros">Nosotros</a>
-            </li>
-
-            <li class="dropdown">
-              <a href="galeria">Galeria</a>                 
-            <li>
-              <a href="contacto">Contacto</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </header>
-    <!--header end-->
+    <?php include('header.php'); ?>
 
     <!--breadcrumbs start-->
     <div class="breadcrumbs">
@@ -537,131 +505,7 @@
 
     <!--container end-->
 
-<!--footer start-->
-    <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-sm-3 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".1s">
-            <h1>
-              Información de Contacto
-            </h1>
-            <address>
-              <p><i class="fa fa-home pr-10"></i>Dirección: San Pedro 5 #712</p>
-              <p><i class="fa fa-globe pr-10"></i>Valdivia, Región de los Rios </p>
-              <p><i class="fa fa-mobile pr-10"></i>Celular : +56 9 92653875 </p>
-              <p><i class="fa fa-mobile pr-10"></i>Celular : +56 9 90699373 </p>
-              <p><i class="fa fa-envelope pr-10"></i>Email :   <a href="javascript:;">gypnetwork.info@gmail.com</a></p>
-            </address>
-          </div>
-          <div class="col-lg-3 col-sm-3 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
-            <h1>Últimos tweet</h1>
-              <div class="tweet-box">
-                <i class="fa fa-twitter"></i>
-                <em>
-                  Siguenos en twitter como
-                  <a href="https://twitter.com/gypnetwork">@gypnetwork</a>
-                  para encontrar todas nuestras novedades!
-                  <a href="https://twitter.com/gypnetwork">twitter.com/gypnetwork</a>
-                </em>
-              </div>
-              <!--
-              <div class="tweet-box">
-                <i class="fa fa-twitter"></i>
-                <em>
-                  Please follow
-                  <a href="javascript:;">@example</a>
-                  for all future updates of us!
-                  <a href="javascript:;">twitter.com/gypnetwork</a>
-                </em>
-              </div>
-              <div class="tweet-box">
-                <i class="fa fa-twitter"></i>
-                <em>
-                  Please follow
-                  <a href="javascript:;">@example</a>
-                  for all future updates of us!
-                  <a href="javascript:;">twitter.com/gypnetwork</a>
-                </em>
-              </div>
-              -->
-          </div>
-          <div class="col-lg-3 col-sm-3">
-            <div class="page-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">
-              <h1>
-                Nuestra Empresa
-              </h1>
-              <ul class="page-footer-list">
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="nosotros">Sobre Nosotros</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="productos">Soporte</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="productos">Servicios</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="productos">Productos</a>
-                </li>
-                <!--
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="career.html">We are Hiring</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="terms.html">Term & condition</a>
-                </li>
-              -->
-              </ul>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-3">
-            <div class="text-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".7s">
-              <h1>
-                Gypnetwork Limitada
-              </h1>
-              <p>
-                No dudes en contactarnos y solicitar tu presupuesto.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <!-- footer end -->
-    
-    <!--small footer start -->
-    <footer class="footer-small">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-sm-6 pull-right">
-                    <ul class="social-link-footer list-unstyled">
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a href="https://www.facebook.com/gypnetwork.info"><i class="fa fa-facebook"></i></a></li>
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".5s"><a href="https://www.twitter.com/gypnetwork"><i class="fa fa-twitter"></i></a></li>
-                        <!--
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".2s"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".3s"><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".4s"><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".6s"><a href="#"><i class="fa fa-skype"></i></a></li>
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".7s"><a href="#"><i class="fa fa-github"></i></a></li>
-                        <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".8s"><a href="#"><i class="fa fa-youtube"></i></a></li>
-                        -->
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                  <div class="copyright">
-                    <p>&copy; Copyright - Gypnetwork Ltda.</p>
-                  </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!--small footer end-->
+    <?php include('footer.php'); ?>
 
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="js/jquery.js">
@@ -689,6 +533,9 @@
 
 
     <script>
+      $(document).ready(function() {
+        $('#link-about').addClass('active');
+      });
 
       wow = new WOW(
         {
